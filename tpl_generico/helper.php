2e25964124e1de411e00ff8a5c5493a52a895f31
Jorge Demetrio Sobieski Teixeira <10842384+jorgedemetrio@users.noreply.github.com>
2026-05-28 14:47:26 -0300

    Correções

---
+++ tpl_generico/helper.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template generico - helper.
+ *
+ * Mantido por compatibilidade com instalações antigas cujo index.php
+ * ainda executa `require_once 'templates/generico/helper.php'`.
+ *
+ * O index.php atual NÃO depende deste arquivo; ele serve apenas para
+ * impedir o erro fatal "Failed to open stream: helper.php" durante a
+ * transição entre versões publicadas.
+ *
+ * @package   Templates.generico
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
